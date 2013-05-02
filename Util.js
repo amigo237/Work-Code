342cdba0058fb77d56049b30afe11dc8d2171fd7
amigo237 <540908525@qq.com>
2013-05-02 11:43:05 +0800

    test
    test

--- Util.js
+++ Util.js
@@ -2,7 +2,7 @@
 	var Util = {
 		//导入行内样式
 		//cssText: 需要导入的行内样式
-		//example: importStyle('.page-content { width: 600px; margin: 0 auto!important }')
+		//example: importStyle('.login_link_in a { color: #8f8f8f }')
 		importStyle: function(cssText) {
 			var element = document.createElement('style');
 			element.appendChild(document.createTextNode(cssText));
