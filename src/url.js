3118747b24a9d5424b645f448645d3e7f8c6e37d
amigo237 <540908525@qq.com>
2013-05-13 18:22:38 +0800

    add url, but not finish

---
+++ src/url.js
@@ -0,0 +1,35 @@
+﻿(function( window ) {
+
+	//example url: http://rob:abcd1234@www.example.com/path/index.html?query1=test&silly=willy#test=hash&chucky=cheese
+	var urlParse = /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/;
+		protocol = ,
+		port = ,
+		tld = ,
+		host = ,
+		domain = ,
+		tld = ,
+		path = ,
+		file = ,
+		filename = ,
+		fileext = ,
+		hash = ,
+		para = ;
+
+	var URLParser = function( url ) {
+		if( !( this instanceof URLParser ) ) {
+			var obj = new URLParser( url );
+			return obj;
+		}
+		url = url || location.href;
+		this.url = url;
+		this.domain = this.getDomain();
+	};
+
+	URLParser.prototype.getDomain = function() {
+		var res = urlParse.exec( this.url );
+		Logger.log(res);
+		return res[2];
+	};
+	
+	window["URLParser"] = window.URLParser || URLParser; 
+})( window );
