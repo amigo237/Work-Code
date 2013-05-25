5f07c310b738ebc0c631d3f3aa53e72f9147e120
amigo237 <540908525@qq.com>
2013-05-25 17:19:34 +0800

    修改分页插件的若干bug

--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -10,6 +10,10 @@
 		NEXTPAGE  = -3,
 		LASTPAGE  = -2;
 
+	function hideString(isShow) {
+		return isShow ? "" : "display:none";
+	}
+
 	var Pagination = function (options) {
 		if (!options.total || !options.el || !options.el.nodeType) {
 			throw new Error("Missing parameter");
@@ -23,59 +27,71 @@
 		this._pagenum = options.pagenum || 10;
 		this._total = options.total;
 		this._el = options.el;
-		//防止用户一直在同一个地方不停的调用构造函数初始化化事件
-		this._el.onclick = null;
-		//当前选中页
-		this._currentpage = options.currentPage || 1;
+
 		//规定分页一共显示多少页,至少显示3个
-		this._showPageNum = (options.showPageNum && options.showPageNum >= 3) ? options.showPageNum : 5;				
+		this._showPageNum = (options.showPageNum && options.showPageNum >= 3) ? options.showPageNum : 5;
+
+		//是否显示首页，末页两个按钮
+		this._isShowFrontPageBtn = options.isShowFrontPageBtn !== undefined ? !!options.isShowFrontPageBtn : true;
+
+		//是否显示总页数
+		this._isShowTotalPageText = options.isShowTotalPageText !== undefined ? !!options.isShowTotalPageText : true;
+
+		//点击翻页按钮的回调函数
 		this._callback = options.callback || function(){};
+
+		//分页的总页数
 		this._totalpage = Math.ceil(this._total/this._pagenum);
+
+		//当前选中页
+		this._currentPage = options.currentPage || 1;
+		this._currentPage = this._currentPage > this._totalpage ? this._totalpage : this._currentPage;
 		if(this._showPageNum > this._totalpage) {
 			this._showPageNum = this._totalpage;
 		}
+
 		//当前分页中的最大页
-		this._currentMaxPage = 0;		
+		this._currentMaxPage = 0;	
+
 		//当前分页中的最小页	
 		this._currentMinPage = 0;
+
 		//步长
 		this._moveStepSize = options.moveStepSize || 3;
 		this._moveStepSize = this._moveStepSize > this._showPageNum ? this._showPageNum - 1 : this._moveStepSize;
 		this._init();
-		if(this._currentpage !== 1) {
-			this._freshUI(this._currentpage);
-		}
+		this._freshUI(this._currentPage);
 	}
 	
-	Pagination.prototype._gotoPage = function(clickpage, tarObj) {
+	Pagination.prototype.gotoPage = function(clickpage) {
 		var tarpage = 0;
-		if (clickpage == this._currentpage) {
+		if (clickpage == this._currentPage) {
 			return;
 		}
 		if (clickpage == NEXTPAGE) {
-			tarpage = this._currentpage + 1;
-			if (tarpage > this._totalpage || tarpage == this._currentpage)
+			tarpage = this._currentPage + 1;
+			if (tarpage > this._totalpage || tarpage == this._currentPage)
 				return;
 		}
 		else if (clickpage == PREPAGE) {
-			tarpage = this._currentpage - 1;
-			if(tarpage < 1 || tarpage == this._currentpage)
+			tarpage = this._currentPage - 1;
+			if(tarpage < 1 || tarpage == this._currentPage)
 				return;
 		}
 		else if (clickpage == FRONTPAGE){
 			tarpage = 1;
-			if(tarpage == this._currentpage)
+			if(tarpage == this._currentPage)
 				return;
 		}
 		else if (clickpage == LASTPAGE){
 			tarpage = this._totalpage;
-			if(tarpage == this._currentpage)
+			if(tarpage == this._currentPage)
 				return;
 		}
 		else{
 			tarpage = clickpage;
 		}
-		this._currentpage = tarpage;
+		this._currentPage = tarpage;
 		if (util.isNumber(tarpage)) {
 			this._callback(tarpage);
 		}
@@ -85,11 +101,17 @@
 	Pagination.prototype._init = function(){
 		var obj = this._el;
 		var self = this;
-		var content = "<span id='pagination-page'>";
-		this._currentMinPage = this._currentpage;
-		var i = (this._currentpage + this._showPageNum) > this._totalpage ? this._totalpage - this._showPageNum + 1 : this._currentpage;
+		var content = "";
+		content += "<span id='pagination-frontgap' style='display:none'>";
+		content += "<a data-page='1' href='javascript:void(0)'>1</a>";
+		content += "<a data-page='2' href='javascript:void(0)'>2</a>";
+		content += "<span id='pagination-frontgap-ellipsis' class='gap'>...</span>";
+		content += "</span>";
+		content += "<span id='pagination-page'>";
+		var i = (this._currentPage + this._showPageNum) > this._totalpage ? this._totalpage - this._showPageNum + 1 : this._currentPage;
+		this._currentMinPage = i;
 		for (var j = i + this._showPageNum; i < j; i++) {
-			if (i == this._currentpage) {
+			if (i == this._currentPage) {
 				content +=  "<a data-page='" + i + "' href='javascript:void(0)' class='on'>" + i + "</a>";
 			}
 			else {
@@ -97,43 +119,56 @@
 			}
 		}
 		this._currentMaxPage = i - 1;
-		content +=  "</span>";
-		content +=  "<a data-page='" + NEXTPAGE + "' href='javascript:void(0)'>下一页</a>";
-		content +=  "<a data-page='" + LASTPAGE + "' href='javascript:void(0)'>末页</a>" + "<strong>共" + this._totalpage + "页</strong>";
+		content += "</span>";
+		content += "<span id='pagination-backgap' style='display:none'>";
+		content += "<span id='pagination-backgap-ellipsis' class='gap'>...</span>";
+		content += "<a data-page='" + (this._totalpage - 1) + "' href='javascript:void(0)'>" + (this._totalpage - 1) + "</a>";
+		content += "<a data-page='" + this._totalpage + "' href='javascript:void(0)'>" + this._totalpage + "</a>";
+		content += "</span>";
+		content += "<a data-page='" + NEXTPAGE + "' href='javascript:void(0)'>下一页</a>";
+		content += "<a data-page='" + LASTPAGE + "' href='javascript:void(0)' style='" + hideString(this._isShowFrontPageBtn) + "'>末页</a>" + "<span style='" + hideString(this._isShowTotalPageText) + "'>共" + this._totalpage + "页</span>";
 		content += "</div>";
 		content = "<a data-page='" + PREPAGE + "' href='javascript:void(0)'>上一页</a>".concat(content);
-		content = "<a data-page='" + FRONTPAGE + "' href='javascript:void(0)'>首页</a>".concat(content);
+		content = "<a data-page='" + FRONTPAGE + "' href='javascript:void(0)' style='" + hideString(this._isShowFrontPageBtn) + "'>首页</a>".concat(content);
 		content = "<div class='pagination'>".concat(content);
 		obj.innerHTML = content;
-		// util.addEvent(this._el, "click", function(e){
-			// e = e || window.event;
-			// var tar = util.getTarget(e);
-			// var page = parseInt(tar.getAttribute("data-page"), 10);
-			// if ( Object.prototype.toString.call(page) === "[object Number]") {
-				// self._gotoPage(page,tar);
-			// }
-			// return false;
-		// });
-		
+		//防止多次重复绑定事件
+		this._el.onclick = null;
 		this._el.onclick = function(e) {
 			e = e || window.event;
 			var tar = util.getTarget(e);
 			var page = parseInt(tar.getAttribute("data-page"), 10);
 			if (util.isNumber(page) && !util.isNaN(page)) {
-				self._gotoPage(page,tar);
+				self.gotoPage(page);
 			}
 			return false;
 		};
 	}
 	
 	//点击了页按钮跳转之后，刷新UI
-	Pagination.prototype._freshUI = function(tarpage){
-		var father = this._el;
-		var child = document.getElementById("pagination-page").childNodes;
-		var length = child.length;
+	Pagination.prototype._freshUI = function(tarpage) {
+		var father = this._el,
+			child = util.id("pagination-page").childNodes,
+			length = child.length;
+
+		//判断是否要显示最后和最前的几页
+		function initPageGap() {
+			var frontgap = util.id("pagination-frontgap"),
+				backgap = util.id("pagination-backgap"),
+				frontEllipsis = util.id("pagination-frontgap-ellipsis"),
+				backEllipsis = util.id("pagination-backgap-ellipsis");
+			this._currentMinPage <= 2 ? frontgap.style.display = "none" : frontgap.style.display = "inline";
+			this._currentMinPage == 3 ? frontEllipsis.style.display = "none" : frontEllipsis.style.display = "inline";
+			this._currentMaxPage >= this._totalpage - 1 ? backgap.style.display = "none" : backgap.style.display = "inline";
+			this._currentMaxPage + 1 == this._totalpage - 1 ? backEllipsis.style.display = "none" : backEllipsis.style.display = "inline";
+		}
+
 		if (tarpage > this._currentMinPage && tarpage < this._currentMaxPage) {
 			for (var i = 0; i < length; i++) {
 				var obj = child[i];
+				if (obj.nodeType !== 1) { 
+					break; 
+				}
 				var text = obj.innerHTML;
 				if (child[i].getAttribute("data-page") == tarpage) {
 					obj.className = "on";
@@ -142,6 +177,7 @@
 					obj.className = "";
 				}
 			}
+			(util.bind(initPageGap, this))();
 		}
 		
 		//点击的是最左边的最小页或者是首页
@@ -153,8 +189,8 @@
 			this._currentMinPage = startIndex;		
 			for (var i = 0; i < this._showPageNum && i < this._totalpage; i++) {
 				var obj = child[i];
-				if(typeof obj == 'undefined'){
-					break;
+				if (obj.nodeType !== 1 || typeof obj == 'undefined') { 
+					break; 
 				}
 				else{
 					obj.innerHTML = startIndex;
@@ -169,11 +205,12 @@
 				}
 			}
 			this._currentMaxPage = startIndex - 1;
+			(util.bind(initPageGap, this))();
 			return;
 		}
 		
 		//点击的是最右边的最大页或者是末页
-		if (tarpage == this._currentMaxPage || tarpage == this._totalpage) {
+		if (tarpage >= this._currentMaxPage || tarpage == this._totalpage) {
 			var endIndex = tarpage + this._moveStepSize;
 			while (endIndex > this._totalpage){
 				endIndex -= 1;
@@ -181,8 +218,8 @@
 			this._currentMaxPage = endIndex;
 			for(var i = endIndex - 1, j = this._showPageNum - 1; j >= 0 && i >= 0; i--,j--){
 				var obj = child[j];
-				if(typeof obj == 'undefined'){
-					break;
+				if (obj.nodeType !== 1 || typeof obj == 'undefined') { 
+					break; 
 				}
 				else{
 					obj.innerHTML = endIndex;
@@ -197,6 +234,7 @@
 				}
 			}
 			this._currentMinPage = endIndex + 1;
+			(util.bind(initPageGap, this))();
 			return;
 		}
 	}
@@ -205,7 +243,8 @@
 		options = options || {};
 		this._pagenum = options.pagenum || this._pagenum;
 		this._total = options.total || this._total;
-		this._currentpage = options.currentPage || this._currentpage;
+		this._currentPage = options.currentPage || this._currentPage;
+		this._currentPage = this._currentPage > this._totalpage ? this._totalpage : this._currentPage;
 		this._showPageNum = options.showPageNum || this._showPageNum;
 		this._callback = options.callback || this._callback;
 		this._totalpage = Math.ceil(this._total/this._pagenum);
@@ -215,15 +254,16 @@
 		this._moveStepSize = options.moveStepSize || this._moveStepSize;
 		this._moveStepSize = this._moveStepSize > this._showPageNum ? this._showPageNum - 1 : this._moveStepSize;
 		this._el.innerHTML = "";
-		this._el.onclick = null;
 		this._init();
-		if(this._currentpage !== 1) {
-			this._freshUI(this._currentpage);
-		}
+		this._freshUI(this._currentPage);
 	}
 	
 	//util
 	var util = {
+		id: function(id) {
+			return document.getElementById(id);
+		},
+
 		addEvent: function(elem, type, func) {
 			if(elem.addEventListener){
 				elem.addEventListener(type, func, false);
@@ -252,6 +292,17 @@
 
 		isNaN: function(obj) {
 			return this.isNumber(obj) && obj != obj;
+		},
+
+		bind: function(func, context){
+			if (Function.prototype.bind) {
+				return func.bind(context);
+			}
+			else {
+				return function(){
+					return func.apply(context, Array.prototype.slice.call(arguments, 0));
+				};
+			}
 		}
 	};
 	
