bb37c20ab91ff062e0dbd580cacd4d3d3f83393d
amigo237 <540908525@qq.com>
2013-08-09 16:53:10 +0800

    增加轮播图的slide和fade模式

--- test/jqSlider/src/jqSlider.js
+++ test/jqSlider/src/jqSlider.js
@@ -1,4 +1,9 @@
-﻿(function ($) {
+﻿/*!
+ * jqSlider v1.0.0
+ * Copyright (c) 2013, in shenzhen. luzhao@xunlei.com
+ */
+
+(function ($) {
     "use strict"
     
 	var Slider = function (el, options) {
@@ -7,8 +12,10 @@
             speed: 600,
 			delay: 5000,
 			item: ".slider-item",
+			container: ".slider-container",
             animation: "fade",
-            startIndex: 0
+            startIndex: 0,
+            complete: $.noop
         };
 		
 		options = $.extend(defaults, options);
@@ -21,7 +28,10 @@
 		constructor: Slider,
 		
 		_init: function(el, options) {
-            var self = this;
+            var self = this,
+                items = $(options.item),
+                container = $(options.container);
+            
             this._el = $(el);
 			this._options = options;
 			this._showItemIndex = options.startIndex;
@@ -32,7 +42,11 @@
             }
             
             if (options.animation === "fade") {
-                $(".slider-item").css("marginRight", "-100%").slice(1).css("opacity", 0);
+                items.css({"marginRight" : "-100%", "float" : "left"}).slice(1).css("opacity", 0);
+            }
+            else if (options.animation === "slide") {
+                items.eq(0).css("left", 0);
+                items.css("position", "absolute").slice(1).css("left", container.width());
             }
             
 			this.play();
@@ -51,7 +65,7 @@
             var self = this;
             
 			this._timerId = setInterval(function() {
-                self.to();
+                self._to();
 			}
 			, this._options.delay);
             
@@ -65,41 +79,81 @@
         
         next: function() {
             var index = (this._showItemIndex + 1) % this._totalItem;
-            this.stop().to(index);
+            this.stop()._to(index, true);
             return this;
         },
         
         previous: function() {
             var index = this._showItemIndex - 1;
             index = index < 0 ? (this._totalItem - 1) : index;
-            this.stop().to(index);
+            this.stop()._to(index, true);
             return this;
         },
         
-        to: function(toIndex) {
-            var self = this;
-            var index = self._showItemIndex,
-                totalItem = self._totalItem;
+        move: function(index) {
+        
+            //index超过slider的总个人，直接返回
+            if (index >= this._totalItem) {
+                return this;
+            }
+            
+            //当前显示的slider和将要显示的slider一样则直接返回
+            if (index === this._showItemIndex) {
+                return this;
+            }
+            this.stop()._to(index, true);
+        },
+        
+        _to: function(toIndex, isNeedCallPlayFunc) {
+            var self = this,
+                index = self._showItemIndex,
+                totalItem = self._totalItem,
+                options = self._options,
+                animation = options.animation,
+                items = $(options.item),
+                cssFrom = {},
+                cssTo = {},
+                container = $(options.container),
+                originalToIndex = toIndex;
+                
             toIndex = toIndex !== undefined ? toIndex % totalItem : (index + 1) % totalItem;
             
-            if (!$(".slider-item").eq(index).queue("fx").length) {
-                $(".slider-item").eq(index).animate(
-                    {opacity: 0},
-                    {duration: self._options.speed}
+            if (!items.eq(index).queue("fx").length) {
+                if (animation === "fade") {
+                    cssFrom["opacity"] = 0;
+                    cssTo["opacity"] = 1;
+                }
+                else if (animation === "slide") {
+                    if (index < toIndex || originalToIndex === undefined) {
+                        items.eq(toIndex).css("left", container.width());
+                        cssFrom["left"] = -container.width();
+                        cssTo["left"] = 0;
+                    }
+                    else {
+                        items.eq(toIndex).css("left", -container.width());
+                        cssFrom["left"] = container.width();
+                        cssTo["left"] = 0;
+                    }
+                }
+                
+                items.eq(index).animate(
+                    cssFrom,
+                    {duration: options.speed}
                 );
                 
-                $(".slider-item").eq(toIndex).animate(
-                    {opacity: 1},
+                items.eq(toIndex).animate(
+                    cssTo,
                     {
-                        duration: self._options.speed,
+                        duration: options.speed,
                         complete: function() {
                             self._showItemIndex = toIndex;
+                            options.complete(self._showItemIndex);
+                            isNeedCallPlayFunc && self.play();
                         }
                     }
                 );
             }
 
-            
             return this;
         }
 	};
