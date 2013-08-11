1b49664a64b42980072fca6da556af98976bb785
amigo237 <540908525@qq.com>
2013-08-11 22:18:20 +0800

    完善jqValidate

--- src/jqValidate.js
+++ src/jqValidate.js
@@ -6,6 +6,12 @@
 (function ($) {
     "use strict"
     
+    var message = {
+        required: '不能为空。',
+        minLength: '字符长度不够。',
+        maxLength: '字符超长。'
+    };
+    
     var numericRegex = /^[0-9]+$/;
         
     var validate = {
@@ -30,34 +36,67 @@
         },
     };
     
-    $.fn.validate = function (type, condition) {
+    $.fn.validate = function (type) {
         var result,
+            condition,
             value = $.trim(this.val()),
             func = validate[type];
         
-        if ($.isFunction(func)) {
-            result = func(value, condition);
-        }
-        else {
+        if (!$.isFunction(func)) {
             throw new Error("can't find this validate type:" + type);
         }
+        
+        switch (type) {
+            case "minLength":
+                var minLength = this.attr("data-min-length");
+                if (minLength === undefined || minLength === null) {
+                    throw new Error("can't find attribute data-min-length");
+                }
+                result = func(value, minLength);
+                break;
+                
+            case "maxLength":
+                var maxLength = this.attr("data-max-length");
+                if (maxLength === undefined || maxLength === null) {
+                    throw new Error("can't find attribute data-max-length");
+                }
+                result = func(value, maxLength);
+                break;
+                
+            default:
+                //"no any other condition, direct call validate function";
+                result = func(value);
+        };
         
         return result;
     };
     
-    $.fn.monitor = function (type, condition, error) {
-        error = $.isFunction(condition) ? condition : error;
-        error = $.isFunction(error) ? error : $.noop;
+    $.fn.monitor = function (type, callback) {
+        callback = $.isFunction(callback) ? callback : $.noop;
         
         var typeSeparator = /\s+/,
             $this = this;
         
-        this.on("change", function() {
-            !$this.validate(type, condition) && error();
+        this.on("change.validate", function() {
+            var typeArr = type.split(typeSeparator),
+                result = {
+                    code: "success",
+                    msg: []
+                };
+                
+            for (var i = 0, j = typeArr.length; i < j; i++) {
+                if (!$this.validate(typeArr[i])) {
+                    result.code = "fail";
+                    result.msg.push({
+                        type: typeArr[i],
+                        message: message[typeArr[i]]
+                    });
+                }
+            }
+            
+            callback(result);
         });
         
         return this;
     };
-    
-    
 })(jQuery)
