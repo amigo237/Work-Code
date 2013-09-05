db4560d00b149e0ba28381a62b77b0b2ef8b3a6f
amigo237 <540908525@qq.com>
2013-09-05 12:52:06 +0800

    加入input事件支持

--- src/jqValidate.js
+++ src/jqValidate.js
@@ -3,6 +3,74 @@
  * Copyright (c) 2013, in shenzhen. luzhao@xunlei.com
  */
 
+ var onInputChange = (function($){
+    
+    /*
+    ie9 doesn't trigger oninput event when content is removed with BACKSPACE, ctrl+x etc... 
+    I will not bother with feature check.
+    */
+    var onInputSupport = 'oninput' in document.createElement('input');
+
+    function OnInputChange(element, callback, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.value = element.value;
+        this._callback = callback;
+        this.options = $.extend({
+            time: 100
+        }, options);        
+        $(element).on('focus', $.proxy(this._listen, this));
+        $(element).on('blur', $.proxy(this._unlisten, this));
+    }
+
+    OnInputChange.prototype = {
+        
+        _listen: function(){
+            if(onInputSupport){
+                this.$element.on('input', $.proxy(this._run, this));
+            }
+            else {
+                this._interval = window.setInterval($.proxy(this._check, this), this.options.time);
+            }
+            return true;
+        },
+        
+        _unlisten: function(){
+            if(onInputSupport){
+                this.$element.off('input', this._run);
+            }
+            else {
+                window.clearInterval(this._interval);
+            }
+            return true;
+        },
+        
+        _run: function(){
+            this.value = this.element.value;
+            var callback = $.proxy(this._callback, this.element);
+            callback(this.value);
+        },
+        
+        _check: function(){
+            if(this.element.value != this.value) {
+                this._run();
+            }
+        }
+    };
+    
+    $.fn.onInputChange = function(callback, options){
+        return this.each(function(){
+            new OnInputChange(this, callback, options);
+        });
+    };
+    
+    return {
+        Constructor: OnInputChange,
+        support:onInputSupport
+    };
+    
+})(window.jQuery);
+
 ;(function ($) {
     "use strict"
     
