4949432d89ea192c5080e740dd2ac1a1f9eba702
amigo237 <540908525@qq.com>
2014-02-13 18:59:31 +0800

    拖动示例

---
+++ widgets/drag/drag.js
@@ -0,0 +1,28 @@
+﻿(function(global, $) {
+    $.fn.drag = function() {
+        this.each(function() {
+            var $this = $(this),
+            x,y,
+            originalX, originalY;
+        
+            function mousemoveHandler(e) {
+                $this.css("left", parseFloat(originalX, 10) + (e.pageX - x));
+                $this.css("top", parseFloat(originalY, 10) + (e.pageY - y));
+            };
+            
+            $this.on("mousedown", function(e) {
+                x = e.pageX;
+                y = e.pageY;
+                originalX = $this.css("left");
+                originalY = $this.css("top");
+                $(document).on("mousemove", mousemoveHandler);
+            });
+            
+            $(document).on("mouseup", function() {
+                $(document).off("mousemove", mousemoveHandler);
+            });
+        });
+        
+        return this;
+    };
+})(window, jQuery);
